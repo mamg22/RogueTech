92b3af1a8d5a32c83e617639442458c84c0affb4
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-09-04 00:55:15 -0400

    Add some basic api

--- api.py
+++ api.py
@@ -1,24 +1,67 @@
+import datetime
+import json
+
 from flask import Blueprint, request, jsonify
 
 import core
 
-api = Blueprint('api', __name__, url_prefix='/api')
+api = Blueprint('api', __name__, url_prefix='/api/v1')
 
 def get_model():
     try:
-        auth_header = request.headers['Authorization']
-    except (KeyError, TypeError):
         token = None
-    try:
-        token_str, token = auth_header.split(" ", 1)
-        assert(token_str == 'token')
-        print(token)
+        if request.authorization:
+            token = request.authorization.token
         return core.Model(token)
     except core.RemovedSessionError as e:
+        # TODO: Return an error
         return core.Model()
 
 
-@api.route('/v1/users')
+@api.route('/users')
 def get_users():
     model = get_model()
     return jsonify(model.get_users())
+
+@api.post('/login')
+def login():
+    data = request.get_json()
+    nombre = data['nombre']
+    clave = data['clave']
+    model = get_model()
+
+    if user := model.login(nombre, clave):
+        return jsonify(user)
+    else:
+        return {"error": "Not ok"}, 400
+
+
+@api.post('/create-account')
+def create_account():
+    data = request.get_json()
+    nombre = data['nombre']
+    clave = data['clave']
+    pin = data['pin']
+
+    model = get_model()
+
+    if model.create_account(nombre, clave, pin):
+        return {"status": "ok"}
+    else:
+        return {"status": "failed"}
+
+@api.post('/score')
+def insert_score():
+    data = request.get_json()
+
+    model = get_model()
+
+    model.insert_score(
+        data['seed'],
+        data['version'],
+        datetime.datetime.fromisoformat(data['date']),
+        data['score'],
+        data['time_ms'],
+        json.dumps(data['details'])
+    )
+    return {}, 200
