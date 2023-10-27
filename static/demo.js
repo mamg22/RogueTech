e772b2e5dea8058e261146cab00ad13685b72353
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-10-27 18:02:07 -0400

    Add wall hole logic

--- static/demo.js
+++ static/demo.js
@@ -143,6 +143,11 @@ class Rectangle {
     width;
     height;
 
+    contains_point(x, y) {
+        return x >= this.x && x < this.x + this.width &&
+               y >= this.y && y < this.y + this.height;
+    }
+
     toString() {
         return `Rectangle((${this.x}, ${this.y}), w: ${this.width}, h: ${this.height})`;
     }
@@ -180,20 +185,17 @@ class Grid {
         const x_dir = Math.sign(dx);
         const y_dir = Math.sign(dy);
 
-        if (dx == 0 && dy == 0) {
-            return;
-        }
-        else if (dx != 0 && dy != 0) {
+        if (dx != 0 && dy != 0) {
             throw Error("Only horizontal and vertical lines supported");
         }
         
         if (dx != 0) {
-            for (let i = 0; i <= dx; i++) {
+            for (let i = 0; i <= Math.abs(dx); i++) {
                 this.set(x0 + (i * x_dir), y0, value);
             }
         }
         else {
-            for (let i = 0; i <= dy; i++) {
+            for (let i = 0; i <= Math.abs(dy); i++) {
                 this.set(x0, y0 + (i * y_dir), value);
             }
         }
@@ -219,7 +221,7 @@ class Grid {
     set_filled_rect(x0, y0, x1, y1, value) {
         const dy = y1 - y0;
         const y_dir = Math.sign(dy);
-        for (let i = 0; i <= dy; i++) {
+        for (let i = 0; i <= Math.abs(dy); i++) {
             const target_y = y0 + (i * y_dir);
             this.set_line(x0, target_y, x1, target_y, value);
         }
@@ -231,7 +233,15 @@ class Grid {
         this.set_filled_rect(rx, ry, rx + rectangle.width - 1, ry + rectangle.height - 1, value)
     }
 
-    content;
+    toString() {
+        const row_strings = Array.from(this.content, function(row) {
+            return row.reduce(function (acc, val) {
+                const symbol = val > 0 ? ' ' : '#'; 
+                return acc + symbol;
+            }, '');
+        });
+        return row_strings.join("\n")
+    }
 }
 
 const GRID_SIZE = 64;
@@ -688,17 +698,20 @@ class BSPNode {
     }
     id;
     rect;
-
     split_offset;
     split_direction;
-
     left;
     right;
+
+    static Direction = {
+        horizontal: 'H',
+        vertical: 'V'
+    }
 
     split(direction, offset) {
         this.split_direction = direction;
         this.split_offset = offset;
-        if (direction == "H" || direction == "h") {
+        if (direction == BSPNode.Direction.horizontal) {
             this.left = new BSPNode(this.id + "0", new Rectangle(
                 this.rect.x,
                 this.rect.y,
@@ -712,7 +725,7 @@ class BSPNode {
                 this.rect.height - offset - 1
             ));
         }
-        else if (direction == "V" || direction == "v") {
+        else if (direction == BSPNode.Direction.vertical) {
             this.left = new BSPNode(this.id + "0", new Rectangle(
                 this.rect.x,
                 this.rect.y,
@@ -728,6 +741,15 @@ class BSPNode {
         }
     }
 
+    get_branches() {
+        if (this.left) {
+            return [this].concat(this.left.get_branches()).concat(this.right.get_branches());
+        }
+        else {
+            return []
+        }
+    }
+
     get_leaves() {
         if (! this.left) {
             return [this]
@@ -735,6 +757,38 @@ class BSPNode {
         else {
             return this.left.get_leaves().concat(this.right.get_leaves());
         }
+    }
+
+    get_wall() {
+        if (this.split_direction == BSPNode.Direction.horizontal) {
+            return new Rectangle(
+                this.rect.x,
+                this.rect.y + this.split_offset,
+                this.rect.width,
+                1
+            );
+        }
+        else if (this.split_direction == BSPNode.Direction.vertical) {
+            return new Rectangle(
+                this.rect.x + this.split_offset,
+                this.rect.y,
+                1,
+                this.rect.height
+            );
+        }
+        else {
+            return null;
+        }
+    }
+
+    get_walls() {
+        if (! this.left) {
+            return [];
+        }
+
+        const my_wall = this.get_wall();
+
+        return [my_wall].concat(this.left.get_walls()).concat(this.right.get_walls())
     }
 
     toString() {
@@ -752,23 +806,115 @@ ${indent}}`
     }
 }
 
-function generate_map() {
-    const root = new BSPNode("0", new Rectangle(0, 0, 22, 10));
+function generate_map(splits) {
+    let grid = new Grid(48, 24);
+    const root = new BSPNode("0", new Rectangle(1, 1, grid.width - 2, grid.height - 2));
 
-    root.split("V", 8);
-    root.left.split("H", 4);
-    root.right.split("V", 7);
-    root.right.left.split("H", 6);
-    root.right.left.right.split("V", 3);
-    root.right.right.split("H", 6);
+    const do_splits = function(node, n) {
+        if (n <= 0) {
+            return;
+        }
+        if (node.rect.width < 5 && node.rect.height < 5) {
+            return;
+        }
 
-    let grid = new Grid(22, 10);
+        let split_dir;
+        if (node.rect.width / node.rect.height > 1.25) {
+            split_dir = BSPNode.Direction.vertical;
+        }
+        else if (node.rect.height / node.rect.width > 1.25) {
+            split_dir = BSPNode.Direction.horizontal;
+        }
+        else {
+            split_dir = chance.bool() ? BSPNode.Direction.horizontal : BSPNode.Direction.vertical;
+        }
+        if (split_dir == BSPNode.Direction.horizontal) {
+            node.split(split_dir, chance.integer({min: 2, max: node.rect.height - 2}));
+        }
+        else {
+            node.split(split_dir, chance.integer({min: 2, max: node.rect.width - 2}))
+        }
+        do_splits(node.left, n - 1 - (chance.bool() ? 1 : 0));
+        do_splits(node.right, n - 1  - (chance.bool() ? 1 : 0));
+    }
+
+    do_splits(root, splits);
+
+    // root.split("V", 8);
+    // root.left.split("H", 4);
+    // root.right.split("V", 7);
+    // root.right.left.split("H", 6);
+    // root.right.left.right.split("V", 3);
+    // root.right.right.split("H", 6);
 
     for (const leaf of root.get_leaves()) {
         grid.set_filled_from_rectangle(leaf.rect, 1);
     }
 
-    console.log(grid);
 
-    return root;
+    for (const branch of root.get_branches()) {
+        let wall = branch.get_wall();
+
+
+        let tried = [];
+        while (true) {
+            let hole_offset;
+            let hole_pos = {x: null, y: null};
+            let neighbor_deltas = [];
+
+            if (branch.split_direction == BSPNode.Direction.horizontal) {
+                hole_offset = chance.natural({min: 0, max: wall.width, exclude: tried});
+                hole_pos.x = wall.x + hole_offset;
+                hole_pos.y = wall.y;
+                neighbor_deltas = [{x: 0, y: -1}, {x: 0, y: 1}];
+            }
+            else {
+                hole_offset = chance.natural({min: 0, max: wall.height, exclude: tried});
+                hole_pos.x = wall.x;
+                hole_pos.y = wall.y  + hole_offset;
+                neighbor_deltas = [{x: -1, y: 0}, {x: 1, y: 0}];
+            }
+
+            let ok_sides = 0;
+            for (const leaf of branch.get_leaves()) {
+                for (const delta of neighbor_deltas) {
+                    if (leaf.rect.contains_point(
+                        hole_pos.x + delta.x,
+                        hole_pos.y + delta.y
+                    )) {
+                        ok_sides += 1;
+                    }
+                }
+            }
+            
+            if (ok_sides == 2) {
+                grid.set(hole_pos.x, hole_pos.y, 1);
+                break;
+            }
+            else {
+                tried.push(hole_offset);
+            }
+        }
+    }
+
+    for (const children of document.querySelectorAll("#map-table div")) {
+        const attrs = children.attributes;
+        try {
+            if (grid.get(attrs.x.value, attrs.y.value) == 0) {
+                children.classList.add("solid");
+            }
+            else {
+                children.classList.remove("solid")
+            }
+        }
+        catch {}
+    }
+
+    map.data = grid.content;
+    map.width = grid.width;
+    map.height = grid.height;
+
+    console.log(root);
+
+    return grid;
 }
