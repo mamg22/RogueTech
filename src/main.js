38aed5fd5f18f3a8d33b55c65c6cc9147cca14a5
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-11-10 12:44:56 -0400

    Add browser version check

--- src/main.js
+++ src/main.js
@@ -24,6 +24,36 @@ if (!CSS.px) {
     }
 }
 
+function check_browser_version() {
+    // Required versions due to structuredClone
+    const MIN_CHROME_VERSION = 98;
+    const MIN_FIREFOX_VERSION = 94;
+
+    const chrome_ver = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+    const firefox_ver = navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+
+    let ok_version = false;
+    if (chrome_ver) {
+        const version_number = +chrome_ver[2];
+        ok_version = version_number >= MIN_CHROME_VERSION;
+        console.log("Got chrome version: " + version_number)
+    }
+    else if (firefox_ver) {
+        const version_number = +firefox_ver[1];
+        ok_version = version_number >= MIN_CHROME_VERSION;
+        console.log("Got firefox version: " + version_number)
+
+    }
+    else {
+        console.warn("Unknown browser, cannot know version")
+        ok_version = true;
+    }
+
+    if (!ok_version) {
+        alert("La versión actual del navegador no es compatible con este juego, por lo que no funcionará correctamente. Por favor, intenta actualizar tu navegador e intentalo de nuevo.");
+    }
+}
+
 function init_game(seed) {
     let game = new Game(seed);
     globalThis.game = game;
@@ -36,6 +66,7 @@ const server_info = JSON.parse(
 globalThis.server_info = server_info
 
 document.addEventListener('DOMContentLoaded', function(e) {
+    check_browser_version();
     init_game();
 });
 window.dispatchEvent(new Event('gameload'))
