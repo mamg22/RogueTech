21dc9d89898428bb8b0cdafb5736d6359cf7460d
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-11-13 14:10:57 -0400

    Fix compilation

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 import Chance from 'chance';
-import { astar, Graph } from '../static/astar';
 import _hyperscript from "hyperscript.org";
 import _template from 'hyperscript.org/src/template';
 
 // Input listeners and handlers
 import './input';
-import { sprites, audios } from './resources';
-import { delay, wait_for, format_ms, clamp,
-         transpose_array, get_move_dir } from './utility';
-import { generate_level } from './level';
 import './game';
 
 const server_info = JSON.parse(
