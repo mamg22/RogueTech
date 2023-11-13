1a9a6db16fd80a8524bd6cb63c505a1f4ee5a939
mamg22 <45301823+mamg22@users.noreply.github.com>
2023-11-13 16:39:06 -0400

    Fix install

--- static/main.js
+++ static/main.js
@@ -2,7 +2,6 @@
 let deferredPrompt;
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevents the default mini-infobar or install dialog from appearing on mobile
-    e.preventDefault();
     // Save the event because you'll need to trigger it later.
     deferredPrompt = e;
     window.deferredPrompt = e;
